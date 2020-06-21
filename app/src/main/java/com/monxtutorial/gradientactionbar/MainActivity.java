0be3fa399d4dfc1386205bc3b90c748491608b18
Irman Firmansyah <irmanfrdev@gmail.com>
2020-06-21 18:32:11 +0700

    Add comment

--- app/src/main/java/com/monxtutorial/gradientactionbar/MainActivity.java
+++ app/src/main/java/com/monxtutorial/gradientactionbar/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // change code
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null){
             actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.mygradient));
